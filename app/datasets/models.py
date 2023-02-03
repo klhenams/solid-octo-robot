2cbb24c009b9e05a25bac1431d473da5c95a32a0
Anthony Addae <maaddae.co@gmail.com>
2023-02-03 12:22:59 +0000

    Modify dataset model

--- app/datasets/models.py
+++ app/datasets/models.py
@@ -17,8 +17,14 @@ class Tag(Timer):
     aspect = models.CharField(max_length=255)
     sentiment = models.CharField(max_length=3, choices=Sentiment.choices)
 
+    def __str__(self):
+        return f"Tag <{self.aspect}, {self.sentiment}>"
+
 
 class Dataset(Timer):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     text = models.TextField()
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.text
