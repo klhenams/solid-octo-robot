a9878f017ecb0d41910477521a505833754ff220
Anthony Addae <maaddae.co@gmail.com>
2023-02-04 08:45:39 +0000

    Add tag tests

--- app/datasets/tests/test_urls.py
+++ app/datasets/tests/test_urls.py
@@ -1,6 +1,6 @@
 from django.urls import resolve, reverse
 
-from app.datasets.models import Dataset
+from app.datasets.models import Dataset, Tag
 
 
 def test_dataset_detail(unlabeled_data: Dataset):
@@ -16,3 +16,16 @@ def test_dataset_detail(unlabeled_data: Dataset):
 def test_dataset_list():
     assert reverse("app:dataset-list") == "/app/datasets/"
     assert resolve("/app/datasets/").view_name == "app:dataset-list"
+
+
+def test_tag_detail(tag: Tag):
+    assert (
+        reverse("app:tag-detail", kwargs={"pk": tag.pk})
+        == f"/app/datasets/tags/{tag.pk}/"
+    )
+    assert resolve(f"/app/datasets/tags/{tag.pk}/").view_name == "app:tag-detail"
+
+
+def test_tag_list():
+    assert reverse("app:tag-list") == "/app/datasets/tags/"
+    assert resolve("/app/datasets/tags/").view_name == "app:tag-list"
