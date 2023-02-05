cc80488bc59dcc190295962d706de6d39744e361
Anthony Addae <maaddae.co@gmail.com>
2023-02-05 18:48:21 +0000

    Fix require user in task

--- app/datasets/tasks.py
+++ app/datasets/tasks.py
@@ -5,8 +5,8 @@ from app.datasets.services.file_datasets import csv_to_db
 
 # @shared_task(ignore_result=True, store_errors_even_if_ignored=True)
 @shared_task
-def csv_to_db_task(file_path: str):
+def csv_to_db_task(user, file_path: str):
     try:
-        csv_to_db(file_path)
+        csv_to_db(user, file_path)
     except Exception as err:  # noqa F841
         pass
