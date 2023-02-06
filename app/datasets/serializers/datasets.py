a6c21454ebf273e4f3a208ab3ef75b2374ce948b
Anthony Addae <maaddae.co@gmail.com>
2023-02-06 12:37:19 +0000

    Clear cahce on dataset creation

--- app/datasets/serializers/datasets.py
+++ app/datasets/serializers/datasets.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from rest_framework import serializers
 
 from app.datasets.models import Dataset, Tag
@@ -32,10 +33,16 @@ class NestedDatasetSerializer(DatasetSerializer):
     tags = TagSerializer(many=True, required=False)
 
     def create(self, validated_data):
+
         if "tags" in validated_data.keys():
             tags = TagSerializer(many=True).create(validated_data.pop("tags"))
             dataset = super().create(validated_data=validated_data)
             dataset.tags.add(*tags)
-            # TODO: unset cache
-            return dataset
-        return super().create(validated_data=validated_data)
+        else:
+            dataset = super().create(validated_data=validated_data)
+
+        # We explicitly deleting cache for tags and datasets
+        # because there maybe new additions.
+        dataset_key = f"datasets_{dataset.user.id}"
+        cache.delete(["tags", dataset_key])
+        return dataset
